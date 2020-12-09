c9e593013ddb44a4f551245a61884e0ca7caf920
Nick DeMarchis <65045396+ndemarchis@users.noreply.github.com>
2020-12-09 01:57:52 -0500

    initial commit? is it? idk i don't really know right now, it's too late

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+import React from "react";
+import OpenWeatherKey from "./comps/api"
+import * as utils from "./comps/utils"
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super()
+    this.state = {
+      weatherIsLoaded: false,
+      lat: 40,
+      lon: -77,
+      exclude: "alerts",
+    }
+  }
+
+  componentDidMount() {
+    this.fetchWeather();
+  }
+
+  fetchWeather() {
+    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${this.state.lat}&lon=${this.state.lon}&exclude=${this.state.exclude}&appid=${OpenWeatherKey}`)
+    .then(res => res.json())
+    .then(
+      (result) => {
+        this.setState({
+          weatherData: result,
+          weatherIsLoaded: true,
+        });
+      },
+
+      (error) => {
+        this.setState({
+          isLoaded: true,
+          error
+        });
+      }
+    )
+  }
+
+  render() {
+    if (!this.state.weatherIsLoaded) {
+      return <div />
+    } else {
+    return (
+      <div className="App">
+        <h1>hey</h1>
+        <p>{utils.timeConverter(this.state.weatherData.current.sunset)}</p>
+      </div>
+    )}}
 }
 
 export default App;
